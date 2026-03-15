1b337137cc6411a23243a729ce4a527d7474df14
Jonah Lee <jonah@Jonahs-MacBook-Pro.local>
2026-03-15 16:50:32 +0100

    Inventory view and edit functionality

--- README.md
+++ README.md
@@ -1,2 +1,108 @@
-# merchandise-inventory-manager
-Merchandise inventory manager UI
+# Merchandise Inventory Manager
+
+This project is an inventory management system built using Rust. It utilizes various libraries and frameworks to provide a robust and efficient solution for managing merchandise inventory.
+
+## Project Structure
+
+```
+merchandise-inventory-manager
+├── src
+│   ├── main.rs          # Entry point of the application
+│   ├── handlers         # Contains request handlers for the application
+│   ├── models           # Defines data models used in the application
+│   ├── views            # Contains view templates for rendering responses
+│   ├── config           # Configuration settings for the application
+│   └── utils            # Utility functions and helpers
+├── Cargo.toml           # Cargo configuration file with dependencies
+└── README.md            # Project documentation
+```
+
+## Dependencies
+
+The project includes the following dependencies:
+
+- **Axum**: A web framework for building APIs.
+- **Tokio**: An asynchronous runtime for Rust.
+- **SQLx**: An asynchronous SQL toolkit for Rust, with support for PostgreSQL.
+- **Serde**: A framework for serializing and deserializing Rust data structures.
+- **Askama**: A template rendering library for Rust.
+- **tower-http**: A set of HTTP utilities for Tower.
+- **dotenvy**: A library for loading environment variables from a `.env` file.
+- **tracing**: A framework for instrumenting Rust programs to collect structured, contextual, and async-aware diagnostics.
+- **tracing-subscriber**: A subscriber for the `tracing` framework that provides logging capabilities.
+- **uuid**: A library for generating and working with UUIDs.
+
+## Getting Started
+
+1. Clone the repository:
+   ```
+   git clone <repository-url>
+   cd merchandise-inventory-manager
+   ```
+
+2. Build the project:
+   ```
+   cargo build
+   ```
+
+3. Run the application:
+   ```
+   cargo run
+   ```
+
+## License
+
+This project is licensed under the MIT License. See the LICENSE file for more details.
+
+## Altering the Database
+```
+sqlite3 inventory.db
+```
+
+Create a table:
+```
+CREATE TABLE inventory (
+id INTEGER PRIMARY KEY NOT NULL,
+name TEXT NOT NULL,
+price REAL NOT NULL DEFAULT 0.0,
+image_path TEXT,
+quantity INTEGER NOT NULL DEFAULT 0
+);
+```
+
+Delete a table:
+```
+DROP TABLE inventory;
+```
+
+Add row:
+```
+INSERT INTO inventory (id, name, price, image_path, quantity) VALUES
+('1', 'Bang Yong Guk', 32.50, 'images/byg.jpeg', 10),
+('2', 'Nothing', 7.50, NULL, 5),
+('3', 'Lemon', 3, 'images/lemon.png', 3),
+('4', 'Watermelon', 9, 'image.watermelon.png', 1);
+```
+
+Delete a row:
+```
+DELETE FROM inventory
+WHERE id = '1';
+```
+
+Delete all rows:
+```
+DELETE FROM inventory;
+```
+
+Update a row:
+```
+UPDATE inventory 
+SET price = 9.50
+WHERE id = 4;
+```
+
+Exit:
+```
+.exit
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,239 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    extract::State,
+    // response::Html,
+    routing::get,
+    routing::post,
+    routing::get_service,
+    Router,
+    // Extension,
+    Form,
+    response::Redirect,
+    http::StatusCode,
+};
+use std::net::SocketAddr;
+use tokio;
+use askama::Template;
+use sqlx::SqlitePool;
+use tower_http::services::ServeDir;
+use sqlx::FromRow;
+use serde::Deserialize;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub db: SqlitePool,
+}
+
+#[derive(Debug, Deserialize)]
+struct ItemAddForm {
+    name: String,
+    price: f64,
+    image_path: String,
+    quantity: i32,
+}
+
+
+#[derive(Debug, Deserialize)]
+struct ItemEditForm {
+    id: i32,
+    name: String,
+    price: f64,
+    image_path: String,
+    quantity: i32,
+}
+
+#[derive(Debug, Deserialize)]
+struct ItemDeleteForm {
+    id: i32,
+}
+
+#[derive(Debug, FromRow)]
+struct Item {
+    id: i32,
+    name: String,
+    price: f64,
+    image_path: String,
+    quantity: i32,
+}
+
+// Templates
+#[derive(Template)]
+#[template(path = "home.html")]
+struct HomeTemplate {
+    title: &'static str,
+}
+
+#[derive(Template)]
+#[template(path = "inventory.html")]
+struct InventoryTemplate { title: &'static str, subtitle: &'static str, items: Vec<Item> }
+
+#[derive(Template)]
+#[template(path = "inventory-edit.html")]
+struct InventoryEditTemplate { title: &'static str, subtitle: &'static str, items: Vec<Item> }
+
+
+#[derive(Template)]
+#[template(path = "sales.html")]
+struct SalesTemplate { title: &'static str, subtitle: &'static str }
+
+#[derive(Template)]
+#[template(path = "sale-tool.html")]
+struct SaleToolTemplate { title: &'static str, subtitle: &'static str }
+
+#[derive(Template)]
+#[template(path = "admin.html")]
+struct AdminTemplate { title: &'static str, subtitle: &'static str }
+
+// Handlers
+async fn home() -> impl axum::response::IntoResponse {
+    let template = HomeTemplate { title: "MD Inventory Manager" };
+    axum::response::Html(template.render().unwrap())
+}
+
+async fn inventory(State(state): State<AppState>) -> impl axum::response::IntoResponse {
+    let items = sqlx::query_as::<_, Item>(
+        "SELECT id, name, quantity, price, image_path FROM inventory"
+    )
+    .fetch_all(&state.db)
+    .await
+    .unwrap();
+
+    println!("Fetched items: {:?}", items);
+    let template = InventoryTemplate {
+        title: "MD Inventory Manager",
+        subtitle: "Inventory Overview",
+        items: items,
+    };
+    axum::response::Html(template.render().unwrap())
+}
+
+async fn inventory_edit(State(state): State<AppState>) -> impl axum::response::IntoResponse {
+    let items = sqlx::query_as::<_, Item>(
+        "SELECT id, name, quantity, price, image_path FROM inventory"
+    )
+    .fetch_all(&state.db)
+    .await
+    .unwrap();
+
+    print!("Fetched items: {:?}", items);
+    let template = InventoryEditTemplate {
+        title: "MD Inventory Manager",
+        subtitle: "Edit Inventory",
+        items: items,
+    };
+    axum::response::Html(template.render().unwrap())
+}
+
+
+async fn inventory_add_submit(
+    State(state): State<AppState>,
+    Form(form): Form<ItemAddForm>,
+) -> impl axum::response::IntoResponse {
+    println!("Received add form data: {:?}", form);
+    let price: f64 = form.price as f64;
+    println!("Price: {}", price);
+
+    sqlx::query(
+        "INSERT INTO inventory (name, image_path, price, quantity) VALUES (?, ?, ?, ?)"
+    )
+    .bind(&form.name)
+    .bind(&form.image_path)
+    .bind(price)
+    .bind(form.quantity)
+    .execute(&state.db)
+    .await
+    .unwrap();
+
+    Redirect::to("/inventory-edit")
+}
+
+async fn inventory_edit_submit(
+    State(state): State<AppState>,
+    Form(form): Form<ItemEditForm>,
+) -> impl axum::response::IntoResponse {
+    println!("Received edit form data: {:?}", form);
+    let price: f64 = form.price as f64;
+    println!("Price: {}", price);
+
+    sqlx::query("UPDATE inventory SET name = ?, price = ?, quantity = ?, image_path = ? WHERE id = ?")
+        .bind(&form.name)
+        .bind(price)
+        .bind(form.quantity)
+        .bind(&form.image_path)
+        .bind(&form.id)
+        .execute(&state.db)
+        .await
+        .unwrap();
+
+    Redirect::to("/inventory-edit")
+}
+
+async fn inventory_delete_submit(
+    State(state): State<AppState>,
+    Form(form): Form<ItemDeleteForm>,
+) -> impl axum::response::IntoResponse {
+    println!("Received delete form data: {:?}", form);
+    sqlx::query("DELETE FROM inventory WHERE id = ?")
+        .bind(&form.id)
+        .execute(&state.db)
+        .await
+        .unwrap();
+
+    Redirect::to("/inventory-edit")
+}
+
+async fn sales() -> impl axum::response::IntoResponse {
+    let template = SalesTemplate {
+        title: "MD Inventory Manager",
+        subtitle: "Sale History",
+    };
+    axum::response::Html(template.render().unwrap())
+}
+
+async fn sale_tool() -> impl axum::response::IntoResponse {
+    let template = SaleToolTemplate {
+        title: "MD Inventory Manager",
+        subtitle: "Sale Tool",
+    };
+    axum::response::Html(template.render().unwrap())
+}
+
+async fn admin() -> impl axum::response::IntoResponse {
+    let template = AdminTemplate {
+        title: "MD Inventory Manager",
+        subtitle: "Admin",
+    };
+    axum::response::Html(template.render().unwrap())
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let pool = SqlitePool::connect("sqlite:inventory.db").await?;
+    let state = AppState { db: pool };
+    let static_service = get_service(ServeDir::new("static")).handle_error(|_error| async {
+        (StatusCode::INTERNAL_SERVER_ERROR, "Static file error")
+    });
+
+    let app = Router::new()
+        .route("/", get(home))
+        .route("/inventory", get(inventory))
+        .route("/sales", get(sales))
+        .route("/sale-tool", get(sale_tool))
+        .route("/admin", get(admin))
+        .route("/inventory-edit", get(inventory_edit))
+        .route("/admin/inventory/add", post(inventory_add_submit))
+        .route("/admin/inventory/edit", post(inventory_edit_submit))
+        .route("/admin/inventory/delete", post(inventory_delete_submit))
+        .nest_service("/static", static_service)
+        .with_state(state);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Server running on http://{}", addr);
+
+    // Run the server
+    axum::Server::bind(&addr)
+    .serve(app.into_make_service())
+    .await?;
+
+    Ok(())
 }
